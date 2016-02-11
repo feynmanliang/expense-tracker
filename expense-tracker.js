805c90a5e0783ad3e5efbcfe2509f550cce348e8
Feynman Liang <feynman.liang@gmail.com>
2016-02-11 21:02:31 +0000

    Adds user roles

--- expense-tracker.js
+++ expense-tracker.js
@@ -1,10 +1,29 @@
 // Populate from fixtures if initially empty
-if (Expenses.find({}).count() == 0) {
-  _.each(Fixtures.expenses, (expense) => {
-    const expenseClean = Expenses.schema.clean(expense)
-    Expenses.insert(expenseClean)
-  })
-}
+var users = [
+      {username:"admin", roles:['admin']},
+      {username:"man", roles:['manager']},
+      {username:"user"}
+    ];
+_.each(users, function (user) {
+  var id;
+  if (Meteor.users.find({username: user.username}).count() == 0) {
+    id = Accounts.createUser({
+      username: user.username,
+      password: 'asdf'
+    });
+
+    if (user.roles.length > 0) {
+      Roles.addUsersToRoles(id, user.roles);
+    }
+    _.each(Fixtures.expenses, (expense) => {
+      const cleanExpense = Expenses.schema.clean(expense)
+      Expenses.insert({
+        ...cleanExpense,
+        ownerId: id
+      })
+    });
+  }
+});
 
 if (Meteor.isClient) {
   Meteor.startup(function() {
