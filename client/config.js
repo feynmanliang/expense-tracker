a20f5b2201cdc2c060d3773330d768b0ab0281c8
Feynman Liang <feynman.liang@gmail.com>
2016-02-16 12:55:04 +0000

    Adds semantic and navbar

--- client/config.js
+++ client/config.js
@@ -1,6 +1,13 @@
-Accounts.ui.config({
-  passwordSignupFields: 'USERNAME_ONLY'
-})
+AccountsTemplates.addField({
+    _id: "username",
+    type: "text",
+    displayName: "username",
+    required: true,
+    minLength: 3,
+    errStr: 'error.minChar'
+});
+
+AccountsTemplates.removeField('email');
 
 Accounts.onLogin(() => {
   const redirect = Session.get('redirectAfterLogin');
--- fixtures.js
+++ fixtures.js
@@ -1,12 +1,14 @@
-Meteor.startup(() => {
-  var user = Meteor.users.findOne({username: 'admin'})
-  if (!user) {
-    var userId = Accounts.createUser({
-      username: 'admin',
-      password: 'admin'
-    });
-    user = Meteor.users.findOne(userId);
-  }
-  Roles.addUsersToRoles(user._id, ['admin']);
+if (Meteor.isServer) {
+  Meteor.startup(() => {
+    var user = Meteor.users.findOne({username: 'admin'})
+    if (!user) {
+      var userId = Accounts.createUser({
+        username: 'admin',
+        password: 'admin'
+      });
+      user = Meteor.users.findOne(userId);
+    }
+    Roles.addUsersToRoles(user._id, ['admin']);
 
-})
+  })
+}
