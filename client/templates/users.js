d87137134f2cc6fec996f1467b1b8c923a78a3f8
Feynman Liang <feynman.liang@gmail.com>
2016-02-16 14:13:58 +0000

    Remove aldeed:tabular dependency

--- client/templates/users.js
+++ client/templates/users.js
@@ -1,33 +1,26 @@
 Template.userList.onCreated(function () {
   this.subscribe("Users")
 })
+Template.userList.helpers({
+  tableSettings: function () {
+    return {
+      collection: Meteor.users,
+      rowsPerPage: 25,
+      showFilter: true,
+      enableRegex: true,
+      fields: [
+        { key: 'username', label: 'Username' },
+        { key: 'roles', label: 'Roles' },
+      ],
+      class: "ui celled table",
+    };
+  }
+});
 
 Template.updateUserForm.onCreated(function () {
   this.subscribe("Users")
 })
-
 AutoForm.addHooks('updateUserForm', { onSuccess: () => FlowRouter.go('/user'), });
-
-Template.updateUserCell.events({
-  'click .update': function () {
-    FlowRouter.go('/user/' + this._id);
-  }
-});
-
-
-Template.deleteUserCell.events({
-  'click .delete': function () {
-    Meteor.call('deleteUser', this._id);
-  }
-});
-
-Template.updateUserForm.events({
-  'click .cancel': function(e) {
-    e.preventDefault();
-    FlowRouter.go('/user');
-  }
-});
-
 Template.updateUserForm.helpers({
   roleOptions: function() {
     return {
@@ -39,3 +32,22 @@ Template.updateUserForm.helpers({
     return Meteor.users.findOne(FlowRouter.getParam("userId"));
   }
 });
+Template.updateUserForm.events({
+  'click .cancel': function(e) {
+    e.preventDefault();
+    FlowRouter.go('/user');
+  }
+});
+
+Template.updateUserCell.events({
+  'click .update': function () {
+    FlowRouter.go('/user/' + this._id);
+  }
+});
+
+Template.deleteUserCell.events({
+  'click .delete': function () {
+    Meteor.call('deleteUser', this._id);
+  }
+});
+
