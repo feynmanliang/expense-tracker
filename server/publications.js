a405eda1242d9c2e1f77152c2f1c67b3afb0e81f
Feynman Liang <feynman.liang@gmail.com>
2016-02-16 14:02:43 +0000

    Removes user-expense join pub

--- server/publications.js
+++ server/publications.js
@@ -18,35 +18,3 @@ Meteor.publish('Users', function() {
     return Meteor.users.find(this.userId);
   }
 });
-
-Meteor.publishComposite("tabular_Expenses", function (tableName, ids, fields) {
-  this.unblock();
-
-  return {
-    find: () => {
-      this.unblock();
-
-      if (Roles.userIsInRole(this.userId, ['admin'])) {
-        return Expenses.find({});
-      } else {
-        return Expenses.find(
-          {_id: {$in: ids}, ownerId: this.userId},
-          {fields: fields});
-      }
-    },
-    children: [
-      {
-        find: (expense) => {
-          this.unblock(); // requires meteorhacks:unblock package
-          return Meteor.users.find(
-            {_id: expense.ownerId},
-            {
-              limit: 1,
-              fields: {username: 1},
-              sort: {username: 1}
-            });
-        }
-      }
-    ]
-  };
-});
