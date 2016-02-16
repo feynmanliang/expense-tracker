d5fd423bb093a48f32f7111c6e05cf72d9ec57cd
Feynman Liang <feynman.liang@gmail.com>
2016-02-16 15:58:31 +0000

    Stateful navbar

--- client/templates/navbar.js
+++ client/templates/navbar.js
@@ -1,7 +1,12 @@
 Template.navbar.events({
-    'click .logout': function(event){
-        event.preventDefault();
-        Meteor.logout();
-        FlowRouter.go('/');
-    }
+  'click .logout': (event) => {
+    event.preventDefault();
+    Meteor.logout();
+    FlowRouter.go('/');
+  }
 });
+
+Template.navbar.helpers({
+  isActive: (selection) => selection === Session.get('navbarSelection') ? 'active' : '',
+  isManagerOrAdmin: () => Roles.userIsInRole(Meteor.userId(), ['admin','manager']),
+})
